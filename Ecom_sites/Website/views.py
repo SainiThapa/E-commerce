250780a0bd2aba4bc5e8e2513b2c72f266539779
Saini Thapa <magarprisu4747@gmail.com>
2023-04-12 22:26:31 +0545

    New changes

--- Ecom_sites/Website/views.py
+++ Ecom_sites/Website/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Destination
 
 # Create your views here.
 def home(request):
     # return HttpResponse("<h1>Hello World</h1>")
     return render(request,'home.html')
 
-def index(request):
-    return render(request,'Index.html')
+def Index(request):
+    dest1= Destination()
+    dest1.img='download.png'
+    dests= [dest1,]
+    return render(request,'Index.html',{'dests': dests})
