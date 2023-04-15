3b402c858fcaa82a9c59d6902b4273a40e9c6c92
Saini Thapa <magarprisu4747@gmail.com>
2023-04-15 21:53:43 +0545

    Contact update

--- Ecom_sites/Website/urls.py
+++ Ecom_sites/Website/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns=[
     path('',views.home,name='home'),
     path('Index',views.Index,name='Index'),
-    path('hello',views.hello,name='hello')
+    path('hello',views.hello,name='hello'),
+    path('contact',views.contact,name='contact'),
 
 ]
