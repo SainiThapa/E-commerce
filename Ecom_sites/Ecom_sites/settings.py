4a9b2f72072d3981ef63e9343a009fac70de61da
Saini Thapa <magarprisu4747@gmail.com>
2023-04-26 22:30:32 +0545

    New changes

--- Ecom_sites/Ecom_sites/settings.py
+++ Ecom_sites/Ecom_sites/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Website.apps.WebsiteConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +77,11 @@ WSGI_APPLICATION = 'Ecom_sites.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Ecommerce',
+        'USER':'postgres',
+        'PASSWORD':'1234',
+        'HOST' : 'localhost'
     }
 }
 
@@ -124,5 +128,6 @@ STATICFILES_DIRS=[
 ]
 STATIC_ROOT=os.path.join(BASE_DIR,'assets')
 
+# MEDIA_ROOT=os.path.join(BASE_DIR,'Ecommerce')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
