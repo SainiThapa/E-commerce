ba63dccb7b406f8dda3712f0de34a78405d7a662
Saini Thapa <magarprisu4747@gmail.com>
2023-05-08 23:04:28 +0545

    Products place

--- Ecom_sites/accounts/urls.py
+++ Ecom_sites/accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('decide',views.decide,name='decide'),
+    path('LoginOrSignup',views.decide,name='decide'),
     path('register',views.register,name='register'),
     path('login',views.login,name='login'),
     ]
